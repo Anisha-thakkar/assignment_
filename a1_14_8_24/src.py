cbce240d42a012190f96e6048f29ebf725a76a45
Anisha Thakkar  <anisha.thakkar@einfochips.com>
2024-08-14 09:46:58 -0700

    aaa

--- a1_14_8_24/src.py
+++ a1_14_8_24/src.py
@@ -2,12 +2,11 @@ dict_ = {1:'one',2:'two',3:'three',4:'four',5:'five'}
 
 import csv
 import pandas as pd
-df=pd.read_csv('/home/anisha/Assignment/a1_14_8_24/test2.csv')
+df=pd.read_csv('/home/anisha/Assignment/a1_14_8_24/test1.csv')
 l=df.columns.values.tolist()
 
 if l[0]=='GET':
     print(l[1])
 else:
-    new_dict={l[1]:l[2]}
-    dict_.update(new_dict)
+    dict_.update({l[1]:l[2]})
     print(dict_)
