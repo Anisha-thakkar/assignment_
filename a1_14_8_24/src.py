c2e3dee8ea8807a6d46b7da35ab383e8e475c826
Anisha Thakkar  <anisha.thakkar@einfochips.com>
2024-08-17 12:11:05 -0700

    update&del

--- a1_14_8_24/src.py
+++ a1_14_8_24/src.py
@@ -5,7 +5,13 @@ dict_ = {1:'one',2:'two',3:'three',4:'four',5:'five'}
 def get_fun(str1):
     print(str1)
 def create_fun(str1,str2,d):
-    d.update({str1:str2})
+    d.update({int(str1):str2})
+    print(d)
+def update_fun(str1,str2,d):
+    d[int(str1)]=str2
+    print(d)
+def delete_fun(str1,d):
+    del d[int(str1)]
     print(d)
 
 with open('test1.csv', 'r') as read_obj: 
@@ -16,5 +22,9 @@ with open('test1.csv', 'r') as read_obj:
     for i in range(len(list_of_csv)):
         if(list_of_csv[i][0]=='GET'):
             get_fun(list_of_csv[i][1])
-        else:
+        elif(list_of_csv[i][0]=='CREATE'):
             create_fun(list_of_csv[i][1],list_of_csv[i][2],dict_)
+        elif(list_of_csv[i][0]=='UPDATE'):
+            update_fun(list_of_csv[i][1],list_of_csv[i][2],dict_)
+        else:
+            delete_fun(list_of_csv[i][1],dict_)
