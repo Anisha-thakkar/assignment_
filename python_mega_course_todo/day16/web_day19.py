3b9405ee75eee6c34078c20034c41cfc13d9d061
Anisha Thakkar  <anisha.thakkar@einfochips.com>
2024-08-29 02:45:25 -0700

    day19 delete todo web application

--- python_mega_course_todo/day16/web_day19.py
+++ python_mega_course_todo/day16/web_day19.py
@@ -8,10 +8,17 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
+
 st.title("My todo app")
 st.subheader("this is my todo app")
 st.write("Content is here..........................")
 
-for todo in todos:
-    st.checkbox(todo)
+
+for index,todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 st.text_input(label="",placeholder="Add new todo:",on_change=add_todo,key="new todo")
